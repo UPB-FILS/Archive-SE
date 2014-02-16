1779a3b509cc8c11b67a0d25696a2a5f3db16ebb
Dragos STOICA <dragos_constantin_stoica@yahoo.com>
2014-02-16 16:09:04 +0200

    Clean-up master branch. Move all files to 2013.

--- 2013/TD1/Belmokhtar_ADLANE/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-  public static void main(String[] args) {
-    TestThread t = new TestThread("Ilyas");
-    TestThread t2 = new TestThread(" Adlane");
-    t.start();
-    t2.start();
-  }
-}
--- 2013/TD2/Culic_Ioana_Barbatei_Alexandra/Notifier.java
+++
@@ -1,4 +0,0 @@
-public class Notifier 
-{
-	public boolean isReady;
-}
